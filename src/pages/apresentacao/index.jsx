a1d7b84e0c3045d2b5ef173057e949c49f2629e4
igorfariasoliveira <igor10farias@gmail.com>
2024-04-30 08:58:40 -0300

    final

--- src/pages/apresentacao/index.jsx
+++ src/pages/apresentacao/index.jsx
@@ -4,13 +4,7 @@ import '..//..//estilos/apresentacao.css';
 const SobreMim = () => {
   return (
     <div className="sobre-mim-container">
-      <div className="image-container">
-        <img
-          src='C:\Users\User\Documents\Curso HTML\template-materialui\src\imagens' 
-          alt="Minha Foto"
-          className="profile-image"
-        />
-      </div>
+      <div><img src="" alt="" /></div>
       <div className="text-container">
         <h1>Sobre Mim</h1>
         <p>
@@ -20,6 +14,10 @@ const SobreMim = () => {
           Trabalho na area de TI a 9 anos, más somente na parte de Hardwere. Sempre me interessei em escrever codigos apenas por hobbie, só que um dia decidi transformar em carreira.
           Tenho um pouco de experiência com front-end, porem apenas para projetos próprios.
         </p>
+        <p>Meu linkedin é <a
+      href="https://www.linkedin.com/in/igor-farias-8a721a24a/"
+      target="_blank"
+      >@Igor Farias</a></p>
       </div>
     </div>
   );
